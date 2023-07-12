349a547a030b2ab12eb46b33ebf445c9010581df
maykpaulmk <gerencia@terracomp.hn>
2023-07-12 16:05:18 -0600

    update
    update

--- clinicalpaii1/src/main/java/hn/clinica/data/service/CitasRepository.java
+++
@@ -1,9 +0,0 @@
-package hn.clinica.data.service;
-
-import hn.clinica.data.entity.Citas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CitasRepository extends JpaRepository<Citas, Long>, JpaSpecificationExecutor<Citas> {
-
-}
--- clinicalpaii1/src/main/java/hn/clinica/data/service/PacientesRepository.java
+++
@@ -1,9 +0,0 @@
-package hn.clinica.data.service;
-
-import hn.clinica.data.entity.Pacientes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface PacientesRepository extends JpaRepository<Pacientes, Long>, JpaSpecificationExecutor<Pacientes> {
-
-}
